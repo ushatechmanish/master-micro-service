fffa45e107d8c6247a36a575541384dd4ea699af
Manish Chauhan <manish.chauhan@ushatech.in>
2023-07-16 21:28:18 +0530

    Implement HATEOAS using HAL closes #27

--- src/main/java/in/ushatech/mastermicroservice/controller/UserController.java
+++ src/main/java/in/ushatech/mastermicroservice/controller/UserController.java
@@ -4,9 +4,13 @@ import in.ushatech.mastermicroservice.dao.service.UserDaoService;
 import in.ushatech.mastermicroservice.domain.User;
 import in.ushatech.mastermicroservice.exception.UserNotFoundException;
 import jakarta.validation.Valid;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
 import java.net.URI;
 import java.util.List;
@@ -25,15 +29,21 @@ public List<User> retrieveAllUsers()
     return userDaoService.findAll();
 }
 
+
     @RequestMapping(method= RequestMethod.GET, path="/users/{id}")
-    public User getUser( @PathVariable int id)
+    public EntityModel<User> getUser(@PathVariable int id)
     {
         User user = userDaoService.findOne(id);
         if(user==null)
         {
             throw new UserNotFoundException("id:"+id);
         }
-        return user;
+        EntityModel<User> entityModel = EntityModel.of(user);
+
+        WebMvcLinkBuilder link = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+
+        entityModel.add(link.withRel("all-users"));
+        return entityModel;
     }
     @RequestMapping(method= RequestMethod.DELETE, path="/users/{id}")
     public void deleteUser(@PathVariable int id)
