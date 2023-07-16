97c706bac04258a96df543ecbb2b9322caf37c3c
Manish Chauhan <manish.chauhan@ushatech.in>
2023-07-16 19:32:19 +0530

    Implement internationalization for HelloWorld closes #25

--- src/main/java/in/ushatech/mastermicroservice/controller/HelloWorldController.java
+++ src/main/java/in/ushatech/mastermicroservice/controller/HelloWorldController.java
@@ -1,11 +1,20 @@
 package in.ushatech.mastermicroservice.controller;
 
 import in.ushatech.mastermicroservice.domain.HelloWorldBean;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController
 {
+    private MessageSource messageSource;
+
+    public HelloWorldController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
 
     @RequestMapping(method = RequestMethod.GET,path = "/hello-world")
     public String sayHello()
@@ -23,5 +32,12 @@ public class HelloWorldController
     public HelloWorldBean sayHelloBean(@PathVariable String name)
     {
         return new HelloWorldBean(String.format("Hello World %s",name));
+    }
+    @RequestMapping(method = RequestMethod.GET,path = "/hello-world-i18n")
+    public String sayHelloInternationalizated()
+    {
+        Locale locale= LocaleContextHolder.getLocale();
+        String message = messageSource.getMessage("good.morning.message",null,"Hello World",locale);
+        return message;
     }
 }
