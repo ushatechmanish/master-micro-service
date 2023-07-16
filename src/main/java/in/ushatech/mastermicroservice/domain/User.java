b6d86f2756f9c6b6017cc055d16645f121d9e15b
Manish Chauhan <manish.chauhan@ushatech.in>
2023-07-16 21:35:35 +0530

    Customize Field Names in user closes #28

--- src/main/java/in/ushatech/mastermicroservice/domain/User.java
+++ src/main/java/in/ushatech/mastermicroservice/domain/User.java
@@ -1,5 +1,6 @@
 package in.ushatech.mastermicroservice.domain;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -13,8 +14,11 @@ public class User
 {
 
     private  Integer userId;
+
+    @JsonProperty("user_name")
     @Size(min = 2, message = "The user name should be minimum 2 character length")
     private  String name;
+    @JsonProperty("user_dob")
     @Past(message = "Birth Date should be in the past")
     private  LocalDate birthDate;
 }
