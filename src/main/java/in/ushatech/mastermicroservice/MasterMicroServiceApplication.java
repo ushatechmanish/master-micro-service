b6789817edaccbad5c908c8116d1bcf5d67e881c
Manish Chauhan <manish.chauhan@ushatech.in>
2023-07-16 10:36:43 +0530

    Initial Commit

---
+++ src/main/java/in/ushatech/mastermicroservice/MasterMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package in.ushatech.mastermicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterMicroServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MasterMicroServiceApplication.class, args);
+    }
+
+}
