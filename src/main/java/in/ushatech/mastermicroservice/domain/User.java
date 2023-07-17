1c2035585fbed7fef738dac42c44a9e79c72afa9
Manish Chauhan <manish.chauhan@ushatech.in>
2023-07-17 08:27:58 +0530

    Configure h2 database and also add a data.sql for creating initial data closes #34

--- src/main/java/in/ushatech/mastermicroservice/domain/User.java
+++ src/main/java/in/ushatech/mastermicroservice/domain/User.java
@@ -1,18 +1,26 @@
 package in.ushatech.mastermicroservice.domain;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
 @Data
 @AllArgsConstructor
+@Entity(name ="user_details")
+@NoArgsConstructor
 public class User
 {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private  Integer userId;
 
     @JsonProperty("user_name")
@@ -21,4 +29,5 @@ public class User
     @JsonProperty("user_dob")
     @Past(message = "Birth Date should be in the past")
     private  LocalDate birthDate;
+
 }
