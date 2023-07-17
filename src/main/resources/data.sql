a1a6927aae73f5973e37620b021c92d73ae41def
Manish Chauhan <manish.chauhan@ushatech.in>
2023-07-17 11:29:49 +0530

    Workaround for avoiding primary key violation while creating a new post

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,19 +1,19 @@
 -- BIRTH_DATE  	USER_ID  	NAME
 
 INSERT INTO USER_DETAILS
-VALUES ('1986-05-16',1,'Manish');
+VALUES ('1986-05-16', 1, 'Manish');
 
 INSERT INTO USER_DETAILS
-VALUES ('1988-11-21',2,'Vineet');
+VALUES ('1988-11-21', 2, 'Vineet');
 
 INSERT INTO USER_DETAILS
-VALUES ('1988-02-08',3,'Pratibha');
+VALUES ('1988-02-08', 3, 'Pratibha');
 
-INSERT INTO POST(id,user_id,description)
-VALUES(1,1,'I want to learn Aws');
-
-INSERT INTO POST
-VALUES(2,2,'I want to learn Docker');
+INSERT INTO POST(id, user_id, description)
+VALUES (1001, 1, 'I want to learn Aws');
 
 INSERT INTO POST
-VALUES(3,3,'I want to learn Kubernetes');
+VALUES (2001, 2, 'I want to learn Docker');
+
+INSERT INTO POST
+VALUES (3001, 3, 'I want to learn Kubernetes');
